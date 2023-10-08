b34fe8e2608db77d69119ccb60dde757effad38d
Marc Jou <smyslov@gmail.com>
2023-10-08 09:04:04 +0200

    Add a generic pass

--- day01/day01.ml
+++ day01/day01.ml
@@ -9,26 +9,29 @@ let sum_chunks chunk =
   |> List.map int_of_string
   |> List.fold_left ( + ) 0
 
-let get_max xs = List.fold_left max min_int xs
-
-let take3 = function
-  | a::b::c::_ -> [a; b; c]
-  | _ -> []
-
-let get_max_3 xs =
-  List.sort compare xs
-  |> List.rev
-  |> take3
-  |> List.fold_left ( + ) 0
 
 let all_chunks data =
-  List.map sum_chunks @@ Str.split (Str.regexp "\n\n") data
+  Str.split (Str.regexp "\n\n") data
+  |> List.map sum_chunks
+  |> List.sort compare
+  |> List.rev
+
+let rec take_n n xs =
+  if n < 1 then []
+  else match xs with
+    | h::t -> h::take_n (n - 1) t
+    | _ -> []
+
+let get_max_n n xs =
+  take_n n xs
+  |> List.fold_left ( + ) 0
 
 
 let () =
   let data = slurp "input.txt" in
   let chunks = all_chunks data in
-  Printf.printf "Sum max elf: %d\n" (get_max chunks);
-  Printf.printf "Sum max 3 elves: %d\n" (get_max_3 chunks)
+  Printf.printf "Sum max elf: %d\n" (get_max_n 1 chunks);
+  Printf.printf "Sum max 3 elves: %d\n" (get_max_n 3 chunks);
+  assert ((get_max_n 1 chunks) = 72511)
 
 (* dune exec  --no-print-directory ./day01.exe *)
