d2894c748f2fbd2d792a4621701ca79bab52067d
Marc Jou <smyslov@gmail.com>
2023-11-05 23:22:05 +0100

    Compose a little bit day 6

--- day06/day06.ml
+++ day06/day06.ml
@@ -8,22 +8,22 @@ module SET = Set.Make(Char)
 
 let explode s = List.init (String.length s) (String.get s)
 
+let set_of_str s =
+  let set = SET.empty in
+  List.fold_right SET.add (explode s) set
+
 let rec find_first_group_of len s idx =
   if String.length s < len then
     None
   else
     let subs = String.sub s 0 len in
-    let set = SET.empty in
-    let set = List.fold_right SET.add (explode subs) set in
-    if SET.cardinal set = len then
+    if SET.cardinal (set_of_str subs) = len then
       Some (subs, idx)
     else
       find_first_group_of len (String.sub s 1 (String.length s - 1)) (idx + 1)
 
-
 let find_first_group_of_4 = find_first_group_of 4
 let find_first_group_of_14 = find_first_group_of 14
-
 
 let solve f data len =
   match f data len with
